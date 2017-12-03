95446e69bda47c0507f5864752521955aa312e50
dskym <dskym@naver.com>
2017-12-03 16:04:00 +0900

    updated to use GPU.

--- test_model.py
+++ test_model.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
 
-from torchvision.datasets import ImageFolder
-import torchvision.transforms
+from torch.autograd import Variable
 
 from torch.utils.data import DataLoader
 
-from torch.autograd import Variable
+import torchvision.utils
+from torchvision.datasets import ImageFolder
 
 def discriminator() :
     discriminator_model = nn.Sequential(
@@ -91,79 +91,118 @@ def generate_video(gen_net, mask_net, static_net, z) :
     foreground = gen_net(z_forward)
 
     mask = mask_net(z_forward).expand(1, 3, 32, 64, 64)
+
     background = static_net(z_backword).view(1, 3, 1, 64, 64).expand(1, 3, 32, 64, 64)
 
     video = foreground * mask + background * (1 - mask)
 
     return video
 
-data_transform = torchvision.transforms.Compose([torchvision.transforms.Scale((64,64)), torchvision.transforms.ToTensor()])
+def init_weights(m) :
+    name = type(m)
+
+    if name == nn.Conv3d or name == nn.ConvTranspose2d or name == nn.ConvTranspose3d :
+        m.weight.data.normal_(0.0, 0.01)
+        m.bias.data.fill_(0)
+    elif name == nn.BatchNorm2d or name == nn.BatchNorm3d :
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+#check GPU
+is_gpu = torch.cuda.is_available()
+print(is_gpu)
+
+if is_gpu :
+    dtype = torch.cuda.FloatTensor
+else :
+    dtype = torch.FloatTensor
+
+D = discriminator().type(dtype)
+gen_net, mask_net, static_net = generator()
+
+gen_net = gen_net.type(dtype)
+mask_net = mask_net.type(dtype)
+static_net = static_net.type(dtype)
+
+if is_gpu :
+    real_labels = Variable(torch.ones(1).type(torch.cuda.LongTensor))
+    fake_labels = Variable(torch.zeros(1).type(torch.cuda.LongTensor))
+else :
+    real_labels = Variable(torch.ones(1).type(torch.LongTensor))
+    fake_labels = Variable(torch.zeros(1).type(torch.LongTensor))
+
+criterion = nn.CrossEntropyLoss().type(dtype)
+
+d_optimizer = torch.optim.Adam(D.parameters(), lr=2e-4, betas=(0.5, 0.999))
+g_optimizer = torch.optim.Adam(gen_net.parameters(), lr=2e-4, betas=(0.5, 0.999))
+
+gen_net.apply(init_weights)
+mask_net.apply(init_weights)
+static_net.apply(init_weights)
+
+
+data_transform = torchvision.transforms.Compose([torchvision.transforms.Scale((64,64)), torchvision.transforms.ToTensor(), torchvision.transforms.Normalize([0.0], [1.0])])
 image_data = ImageFolder(root='./testset/', transform=data_transform)
 data_loader = DataLoader(image_data, batch_size=1, shuffle=True)
 
 video_data = None
 
-for data, _ in data_loader :
+for data, _ in data_loader:
     data.unsqueeze_(2)
 
     if torch.is_tensor(video_data):
         video_data = torch.cat((video_data, data), 2)
-    elif video_data == None :
+    elif video_data == None:
         video_data = data
-
-dtype = torch.FloatTensor
 
 video = Variable(video_data)
 
-D = discriminator()
-gen_net, mask_net, static_net = generator()
+for epoch in range(1, 100) :
 
-real_labels = Variable(torch.ones(1).type(torch.LongTensor))
-fake_labels = Variable(torch.zeros(1).type(torch.LongTensor))
+    # 1. Train Discriminator
 
-criterion = nn.CrossEntropyLoss()
+    # 1-1. Real Video
+    outputs = D(video).view(1, 2)
+    d_loss_real = criterion(outputs.data, real_labels)
+    real_score = outputs
 
-d_optimizer = torch.optim.Adam(D.parameters(), lr=3e-3,)
-g_optimizer = torch.optim.Adam(gen_net.parameters(), lr=3e-3)
+    # 1-2. Fake Video
+    z = Variable(torch.randn(100) * 0.01).type(dtype)
+    fake_videos = generate_video(gen_net, mask_net, static_net, z)
+    outputs = D(fake_videos).view(1, 2)
+    d_loss_fake = criterion(outputs, fake_labels)
+    fake_score = outputs
 
+    d_loss = d_loss_real + d_loss_fake
 
-
-
-# 1. Train Discriminator
-
-# 1-1. Real Video
-outputs = D(video).view(1, 2)
-d_loss_real = criterion(outputs.data, real_labels)
-real_score = outputs
-
-# 1-2. Fake Video
-z = Variable(torch.randn(100) * 0.01)
-fake_videos = generate_video(gen_net, mask_net, static_net, z)
-outputs = D(fake_videos).view(1, 2)
-d_loss_fake = criterion(outputs, fake_labels)
-fake_score = outputs
-
-d_loss = d_loss_real + d_loss_fake
-
-D.zero_grad()
-d_loss.backward()
-d_optimizer.step()
+    D.zero_grad()
+    d_loss.backward()
+    d_optimizer.step()
 
 
 
 
 
-# 2. Train Generator
-z = Variable(torch.randn(100) * 0.01)
-fake_videos = generate_video(gen_net, mask_net, static_net, z)
-outputs = D(fake_videos).view(1, 2)
+    # 2. Train Generator
+    z = Variable(torch.randn(100) * 0.01).type(dtype)
+    fake_videos = generate_video(gen_net, mask_net, static_net, z)
+    outputs = D(fake_videos).view(1, 2)
 
-g_loss = criterion(outputs, real_labels)
+    g_loss = criterion(outputs, real_labels)
 
-D.zero_grad()
-gen_net.zero_grad()
-mask_net.zero_grad()
-static_net.zero_grad()
+    D.zero_grad()
+    gen_net.zero_grad()
+    mask_net.zero_grad()
+    static_net.zero_grad()
 
-g_loss.backward()
-g_optimizer.step()
+    g_loss.backward()
+    g_optimizer.step()
+
+print('End Learning')
+
+z = Variable(torch.randn(100) * 0.01).type(dtype)
+
+for i in range(32) :
+    fake_video = torch.squeeze(generate_video(gen_net, mask_net, static_net, z))[:,i,:,:]
+    print(fake_video)
+    torchvision.utils.save_image(tensor=fake_video.data, filename="./test" + str(i+1) + ".png")
