2f81ab3b3ed467e02822d7ac8c412eab9359e6c1
dskym <dskym@naver.com>
2017-12-06 00:38:38 +0900

    create new data loader

--- new_data_loader.py
+++ new_data_loader.py
@@ -13,17 +13,28 @@ class VideoFolder(data.Dataset) :
     def __init__(self, root, transform=None) :
         self.video_path = []
 
+        self.root = root
+        self.transform = transform
+
         self.data_path = list(map(lambda x: os.path.join(root, x), os.listdir(root)))
+
+        self.classes, self.class_to_idx = self.find_classes(root)
 
         for data_path in self.data_path :
             self.video_path = self.video_path + list(map(lambda x: os.path.join(data_path, x), os.listdir(data_path)))
 
-        self.transform = transform
+    def find_classes(self, dir):
+        classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
+        classes.sort()
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        
+        return classes, class_to_idx
 
     def __getitem__(self, index) :
         video_path = self.video_path[index]
         image_path = list(map(lambda x: os.path.join(video_path, x), os.listdir(video_path)))
-
+        label = self.class_to_idx[str(os.path.dirname(video_path)).replace('./dataset/', '')]
+       
         video = None
 
         for image in image_path :                
@@ -39,7 +50,7 @@ class VideoFolder(data.Dataset) :
             elif video == None:
                 video = image
 
-        return video
+        return (video, label)
 
     def __len__(self) :
         return len(self.video_path)
