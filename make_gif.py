a13035e521c290f955ebd4b683524adf848f1763
dskym <dskym@naver.com>
2017-12-06 09:28:50 +0000

    updated

--- make_gif.py
+++ make_gif.py
@@ -3,6 +3,8 @@ import glob
 
 def make_gif(root, output, fps) :
     file_list = glob.glob(root + '/*.png')
+    
+    file_list.sort(key=lambda x: int(x.split('_')[1].split('.')[0]))
 
     clip = mpy.ImageSequenceClip(file_list, fps=fps)
     clip.write_gif(output, fps=fps)
