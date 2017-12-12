de73a2082629b89f4984f258a7c9abfedf55b68c
dskym <dskym@naver.com>
2017-12-12 05:59:08 +0000

    final version

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils import data
 from torchvision import transforms
 from PIL import Image
+import numpy as np
 
 import os
 
@@ -13,19 +14,45 @@ class VideoFolder(data.Dataset) :
     def __init__(self, root, transform=None) :
         self.video_path = []
 
-        self.data_path = list(map(lambda x: os.path.join(root, x), os.listdir(root)))
+        self.root = root
+        self.transform = transform
+
+        self.data_path = list(map(lambda x: os.path.join(self.root, x), os.listdir(self.root)))
+
+        self.classes, self.class_to_idx = self.find_classes(self.root)
 
         for data_path in self.data_path :
             self.video_path = self.video_path + list(map(lambda x: os.path.join(data_path, x), os.listdir(data_path)))
 
-        self.transform = transform
+    def find_classes(self, dir) :
+        classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
+        classes.sort()
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        
+        self.num_classes = len(classes)
+        
+        return classes, class_to_idx
 
     def __getitem__(self, index) :
         video_path = self.video_path[index]
         image_path = list(map(lambda x: os.path.join(video_path, x), os.listdir(video_path)))
-        
-        video = None
 
+        cls = np.array([self.class_to_idx[str(os.path.dirname(video_path)).replace('./dataset/', '')]])
+            
+        tmp = torch.unsqueeze(torch.from_numpy(cls), 0)
+
+        label =  torch.FloatTensor(1, self.num_classes)
+
+        label.zero_()
+
+        label.scatter_(1, tmp, 1)
+
+        label = torch.squeeze(label, 0)
+
+        video = None
+        
+        image_path.sort()
+        
         for image in image_path :                
             image = Image.open(image).convert('RGB')
 
@@ -39,7 +66,7 @@ class VideoFolder(data.Dataset) :
             elif video == None:
                 video = image
 
-        return video
+        return (video, label)
 
     def __len__(self) :
         return len(self.video_path)
